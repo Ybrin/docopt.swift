a15f26f46b4e5dcc24dc2b121d164ce1a835da53
Koray Koska <koray@koska.at>
2017-09-28 23:29:32 +0200

    Swift PM v4 support

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
+// swift-tools-version:4.0
 import PackageDescription
 
 let package = Package(
-    name: "Docopt.swift"
+    name: "Docopt",
+    products: [
+        .library(name: "Docopt", targets: ["Docopt"]),
+    ],
+    targets: [
+        .target(name: "Docopt", path: "Sources"),
+	.testTarget(name: "DocoptTests", dependencies: ["Docopt"], path: "Tests"),
+    ],
+    swiftLanguageVersions: [3]
 )
